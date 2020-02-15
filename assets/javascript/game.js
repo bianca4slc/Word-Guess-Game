fa8a085fd91f3c4130d94e113d837e913bdbe7ac
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-02-14 23:46:44 -0700

    still working on rows/col added array to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,1 @@
-var theAnimals =["horse", "duck", "rooster", "sheep", "donkey", "cat", "dog", "monkey", "tiger" ]
+var theAnimals = ["horse", "duck", "rooster", "sheep", "donkey", "cat", "dog", "monkey", "tiger"]
