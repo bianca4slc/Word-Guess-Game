6ffe8a7e9bcef186351cdc5013a92c9f3a634d38
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-02-15 22:45:15 -0700

    starting js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,12 @@
-var theAnimals = ["horse", "duck", "rooster", "sheep", "donkey", "cat", "dog", "monkey", "tiger"]
+var theAnimals = ["horse", "duck", "rooster", "sheep", "donkey", "cat", "dog", "monkey", "tiger"];
+
+var animals = theAnimals[Math.floor(Math.random() * words.lenth)];
+
+var answerArray = [];
+for (var i = 0; i < animals.length; i++) {
+    answerArray[i] = "_";
+
+}
+
+var remainingLetter = animals.length;
+
