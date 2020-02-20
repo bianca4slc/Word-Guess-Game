155b87532579f8df7daf94b13c33419aa46087a4
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-02-19 23:49:31 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,115 @@
 // the list of word to guess
 var theAnimals = ["horse", "duck", "rooster", "sheep", "donkey", "cat", "dog", "monkey", "tiger"];
 
-//randomly chooses a word to guess from the variable above
-var animals = theAnimals[Math.floor(Math.random() * theAnimals.length)];
-console.log(animals)
+// randomly chooses a word to guess from the variable above
+var animal = '';
 
 // variable to hold total wins starting at zero
 var wins = 0;
-
+var losses = 0;
 //var for incorrect guesses
 var incorrecGuesses = 8;
 
+var guesses = 0;
+
 var answerArray = [];
 
+var lettersGuessed = [];
+
 // letters remaining in word to be guessed
-var remainingLetters = animals.length;
-
-// var for when a letter is guessed
-var letterGuess = event.key;
-
-
-
-
-//test to see if the var is working
-console.log(Math.floor(Math.random() * theAnimals.length))
-
-//functions to start game, generate spaces and display spaces
-function startGame() {
-    generateSpaces()
-    displaySpaces()
-}
+var remainingLetters = 8;
 
 function generateSpaces(){
-    for (var i = 0; i < animals.length; i++) {
+    for (var i = 0; i < animal.length; i++) {
         answerArray[i] = "_";
     }
 }
 
 function displaySpaces() {
     var answerArrayString = answerArray.join() 
-    var playArea = document.getElementById("game").innerText = answerArrayString
-    console.log(playArea)
+    document.getElementById("game").innerText = answerArrayString
+}
+
+function displayAlreadyGuessed() {
+    var guesses = lettersGuessed.join() 
+    document.getElementById("guessed").innerText = guesses
+}
+
+function displayRemainingGuesses() {
+    document.getElementById("remaining").innerText = incorrecGuesses
+}
+
+function displayWins() {
+    document.getElementById("wins").innerText = wins
+    document.getElementById("losses").innerText = losses
+}
+
+function refreshApp() {
+    displaySpaces();
+    displayAlreadyGuessed();
+    displayRemainingGuesses();
+    displayWins();
+}
+
+//functions to start game, generate spaces and display spaces
+function startGame() {
+    animal = theAnimals[Math.floor(Math.random() * theAnimals.length)];
+    console.log(animal)
+    answerArray = [];
+    lettersGuessed = [];
+    incorrecGuesses = 8;
+    remainingLetters = animal.length
+    generateSpaces();
+    refreshApp();
 }
 
 // starts game 
 document.getElementById("start").addEventListener("click", startGame)
 
 document.onkeyup = function(event) {
+    // var for when a letter is guessed
+    console.log(event);
+    var letterGuess = event.key.toLowerCase();
 
-    for (var j = 0; j< animals.length; j++) {
-        if (animals[j] === letterGuess) {
-            // answerArray[j] = letterGuess;
-            remainingLetters--;
-            console.log(letterGuess)
-        }
-        else {
-            incorrecGuesses--;
-            
+    for (var i = 0; i < lettersGuessed.length; i++) {
+        if(lettersGuessed[i] === letterGuess) {
+            // already guessed this letter, do nothing
+            return;
         }
     }
+
+    lettersGuessed.push(letterGuess);
+
+    var correctGuess = false;
+    for (var j = 0; j< animal.length; j++) {
+        if (animal[j].toLowerCase() === letterGuess) {
+            answerArray[j] = letterGuess;
+            remainingLetters--;
+            guesses++;
+
+            // this letter guessed is correct
+            correctGuess = true;
+        }
+    }
+
+    if (correctGuess === false) {
+        // incorrect guesses decrement by 1
+        incorrecGuesses--;
+    }
+
+    if (remainingLetters === 0 || incorrecGuesses === 0) {
+        // game over
+        if (correctGuess) {
+            // winner
+            wins++;
+            alert('You are a winner!')
+        } else {
+            // loser
+            losses++;
+            alert('You lost.. play again!')
+        }
+        startGame();
+    }
+
+    refreshApp();
 };
-
-
-//display letters already guessed
-
-//update total wins
-
-//win/lose screen
-
-//toLowerCase
-
